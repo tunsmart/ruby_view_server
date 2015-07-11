c832e80aa7cc524b27fc7e2398bf78d78f079c7c
Babatunde Alli <tunsmart@gmail.com>
2015-07-11 04:35:02 +0100

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,14 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = 'aPpLe'
+erb_string = "<h2><%= name.capitalize %></h2>"
+
+template = ERB.new(erb_string)
+puts template.result(binding)
+
+fruits = ["oranges", "apples", "avocado", "banana"]
+template = ERB.new "My best fruit is: <%= fruits.each do |x|  puts x
+                                          end %>"
+puts template.result(binding)
