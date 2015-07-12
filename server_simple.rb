0718e85d5929ec0cfa9723637bd55d0b4bee80eb
Babatunde Alli <tunsmart@gmail.com>
2015-07-12 16:27:19 +0100

    Simple server completed

--- server_simple.rb
+++ server_simple.rb
@@ -11,4 +11,7 @@ server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 trap 'INT' do server.shutdown end
 
 ## Start the server
+server.mount_proc '/' do |req, res|
+  res.body = 'Hello, world!'
+end
 server.start
